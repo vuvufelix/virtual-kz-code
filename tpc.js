f5d629fb7bc6ce170e429e7ff05acf5ba9a395a5
vuvufelix <vuvufelix7@gmail.com>
2025-02-24 09:07:08 -0300

    Atualizado kz

--- tpc.js
+++ tpc.js
@@ -5,10 +5,10 @@ const users = [
         produto: 'Ombreira térmica',
         preco: 15000,
         descricao() {
-            return 'Ombreira termica para ajudar na cura de lezões no corpo'
+            return 'Ombreira termica para ajudar na cura de lezões no corpo';
         },
         valor: 10000,
-        estrela: true
+        estrela: true,
     },
     {
         nome: 'nzuzi teresa',
@@ -16,10 +16,10 @@ const users = [
         produto: 'Tijela elétrica',
         preco: 10500,
         descricao() {
-            return 'Tijela para aquecer a comida em qualquer sítio'
+            return 'Tijela para aquecer a comida em qualquer sítio';
         },
         valor: 6000,
-        estrela: false
+        estrela: false,
     },
     {
         nome: 'dina feliciana',
@@ -27,10 +27,10 @@ const users = [
         produto: 'Corretor de postura',
         preco: 11000,
         descricao() {
-            return 'Ajuda na correção do corpo'
+            return 'Ajuda na correção do corpo';
         },
         valor: 5000,
-        estrela: true
+        estrela: true,
     },
     {
         nome: 'elizabeth miguel',
@@ -38,10 +38,10 @@ const users = [
         produto: 'Adesivo emagrecedor',
         preco: 8000,
         descricao() {
-            return 'Ajuda no emagecimeno da barriga'
+            return 'Ajuda no emagecimeno da barriga';
         },
         valor: 5500,
-        estrela: false
+        estrela: false,
     },
     {
         nome: 'distinto joão',
@@ -49,58 +49,61 @@ const users = [
         produto: 'Saco de carvão',
         preco: 7500,
         descricao() {
-            return 'Para cuzinhar esquentar'
+            return 'Para cuzinhar esquentar';
         },
         valor: 4000,
-        estrela: false
+        estrela: false,
     },
-]
+];
 
-let containerDosProdutos = [
-    
-] 
+let containerDosProdutos = [];
 
-function buscarDadosNoBanco(user) {
-    let value = []
-    for (let data of user) {
-        value.push(Object.values(data))
+function publicarProduto(dados, bancoDeDados) {
+    const usuario = bancoDeDados.find((user) => user.id === dados.id);
+
+    if (!usuario || !dados.id) {
+        return console.log('Usuário não encontrado.');
     }
-    return value.map(element => {return element })
-}
 
-let verificacao = buscarDadosNoBanco(users).map(data => { return data })
+    if (!dados.produto) {
+        return console.log('Adicione um produto para publicar!');
+    }
 
-function publicarProduto(dado, verificar) {
+    if (dados.valor < 5000) {
+        return console.log('Valor de publicação insuficiente!');
+    }
 
-    if(dado.id in verificar) {
-        while (!dado.produto || dado.valor < 5000 || !dado.preco || !dado.descricao) {
-            console.log(`${dado.nome.toUpperCase()} algo deu errado! Dica: Verifique se existe um produto, preço, descrição ou se tens valor suficiente para esta publicação.`)
-            break
-        }
-        const configLanguege = 'pt-br'
-        const configTime = {
-            hour: '2-digit',
-            minute: '2-digit'
-        }
-        let hora = new Date()
+    if (!dados.preco) {
+        return console.log('Adicione o preço do seu produto!');
+    }
 
-        const formatarData = hora.toLocaleDateString(configLanguege, configTime)
+    if (!dados.descricao) {
+        return console.log('Adicione a descrição do seu produto!');
+    }
 
-        let container = Object.create({dado, formatarData})
+    const configLanguage = 'pt-br';
+    const configTime = {
+        hour: '2-digit',
+        minute: '2-digit',
+    };
+    const horaDaPublicacao = new Date().toLocaleDateString(configLanguage, configTime);
 
-        if(dado.estrela === true) {
-            containerDosProdutos.splice(0, 0, container)
-        } if(dado.estrela === false){
-            containerDosProdutos.push(container)
-        }
+    const produto = {
+        ...dados,
+        horaDaPublicacao,
+    };
 
+    if (dados.estrela) {
+        containerDosProdutos.unshift(produto); // Adiciona no início
     } else {
-        console.log('Você não está cadastrado na nossa aplicação. Porfavor cadastre - se!')
+        containerDosProdutos.push(produto); // Adiciona no final
     }
-
 }
 
-buscarDadosNoBanco(users)
-publicarProduto(users[4], verificacao)
+publicarProduto(users[0], users);
+publicarProduto(users[1], users);
+publicarProduto(users[2], users);
+publicarProduto(users[3], users);
+publicarProduto(users[4], users);
 
-console.log(containerDosProdutos)
+console.log(containerDosProdutos);
